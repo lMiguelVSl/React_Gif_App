f2acaae8950ef47866cc8a74c7f5ff71c4ac74f5
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-11 11:15:23 -0500

    Gif app skeleton

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+        </>
+    )
+}
