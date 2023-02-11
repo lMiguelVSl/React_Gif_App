29def3fc3a104caca72fa2ef1274cd78044786d8
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-11 13:13:54 -0500

    Validation input value

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -5,12 +5,13 @@ export const AddCategory = ({ onNewCategorie }) => {
     const [value, setInputValue] = useState('');
 
     const handleInputChange = (event) => {
-        if (event.target.value.trim().lenght <= 1) return;
         setInputValue(event.target.value.trim());
     }
 
     const onSubmit = (event) => {
         event.preventDefault();
+        console.log('value', value);
+        if (value.length <= 1) return;
         onNewCategorie(value);
         setInputValue('');
     }
