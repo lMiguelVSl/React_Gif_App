dd4e95b877afa7c0b242d0921dd990f24bd9370a
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-12 15:00:37 -0500

    Adding Api fetch

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+export const getGifs = async (category) => {
+    const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=RQI7a9UnR0wgafKcyoLoiTbp16asJHL6&q=${category}&limit=10`);
+    const { data } = await res.json();
+
+    const gifsMap = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    console.log('gif', gifsMap);
+    return gifsMap;
+}
